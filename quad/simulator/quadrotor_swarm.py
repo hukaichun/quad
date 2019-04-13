a020726be0536f86d10b61875501d6e2a959f430
kai <hu.kaichun@gmail.com>
2019-04-14 01:18:47 +0800

    modify

--- quad/simulator/quadrotor_swarm.py
+++ quad/simulator/quadrotor_swarm.py
@@ -7,6 +7,10 @@ class QuadrotorSwarm(Swarm):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.set_physical_property()
+
+        self._random_position_radius = 2
+        self._random_velocity_radius = 2
+        self._random_angular_velocity_radius = 6
         
     def set_physical_property(self,
         length = 0.105,
@@ -37,16 +41,69 @@ class QuadrotorSwarm(Swarm):
         self.__thrust_2_force_trans_matrix = thrust_2_force
 
 
-    def apply_force(self, thrust, ext_force = [0,0,0]):
+    def reset(self, idxs=None):
+        if idxs is None:
+            self.reset_all()
+            return
+
+        self.attitude[idxs] = (np.random.random(self.attitude[idxs].shape)-.5)
+        self.position[idxs] *= self._random_position_radius
+        self.angular_velocity[idxs] *= self._random_angular_velocity_radius
+        self.velocity[idxs] *= self._random_velocity_radius
+
+        self.quaternion[idxs] /= np.linalg.norm(self.quaternion[idxs], axis=1, keepdims=True)
+
+
+    def reset_all(self):
+        self.attitude = (np.random.random(self.attitude.shape)-.5)
+        self.position *= self._random_position_radius
+        self.angular_velocity *= self._random_angular_velocity_radius
+        self.velocity *= self._random_velocity_radius
+
+        self.quaternion /= np.linalg.norm(self.quaternion, axis=1, keepdims=True)
+
+
+    def apply_force(self, body_torque, body_force, ext_force = [0,0,0]):
+        self.body_force = body_force
+        self.body_torque = body_torque
+        self.external_force = self.__gravity + ext_force
+
+
+    def apply_thrust(self, thrust):
         general_force = self.__thrust_2_force(thrust)
         torque, force = np.split(general_force, 2, axis=1)
-        self.body_force = force
-        self.body_torque = torque
-        self.external_force = self.__gravity + ext_force
+        self.apply_force(torque, force)
+
 
     #  thrust | (batch, 1, 4) or (1,4)
     def __thrust_2_force(self, thrust):
         return np.matmul(thrust, self.__thrust_2_force_trans_matrix)
+
+
+    @property
+    def random_position_radius(self):
+        return self._random_position_radius
+    
+    @random_position_radius.setter
+    def random_position_radius(self, value):
+        self._random_position_radius = value
+
+    @property
+    def random_velocity_radius(self):
+        return self._random_velocity_radius
+    
+    @random_velocity_radius.setter
+    def random_velocity_radius(self, value):
+        self._random_velocity_radius = value
+
+
+    @property
+    def random_angular_velocity_radius(self):
+        return self._random_angular_velocity_radius
+    
+    @random_angular_velocity_radius.setter
+    def random_angular_velocity_radius(self, value):
+        self._random_angular_velocity_radius = value
 
     @property
     def eval(self):
